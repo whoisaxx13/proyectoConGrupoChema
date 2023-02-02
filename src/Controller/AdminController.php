0b62a09c3016727fb73c284aaa5982c9da47c302
rIftimie <thunderray16@gmail.com>
2023-02-02 16:45:01 +0100

    admin show user report

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,15 +4,55 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
+
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+
+    #[Route('/', name: 'app_admin')]
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'users' => $userRepository->findAll(),
         ]);
     }
+
+    #[Route('/show/{dni}', name: 'app_admin_show', methods:["GET"])]
+    public function show(Request $request, UserRepository $userRepository, $dni): Response
+    {
+        $filter=$request->get("month");
+        $tasksFiltered=[];
+
+        $user = $userRepository->find($dni);
+        $userTasks = $user->getTasks()->getValues();
+
+        if($filter>0 && $filter<13){
+            if($filter>0 && $filter<10){
+                $filter="0".$filter;
+            }
+            for ($i=0; $i < count($userTasks); $i++) { 
+                if($filter===$userTasks[$i]->getStartTime()->format("m")){
+                    $tasksFiltered[]=$userTasks[$i];
+                }
+            }
+        } else {
+            $tasksFiltered = $userTasks;
+        }
+        $arrStart = [];
+        for ($i=0; $i <count($tasksFiltered) ; $i++) { 
+            $arrStart[]= $tasksFiltered[$i]->getStartTime();
+        }
+        array_multisort($arrStart, SORT_ASC, $tasksFiltered);
+
+        return $this->render('admin/show.html.twig', [
+            'user' => $user,
+            'tasks' => $tasksFiltered,
+            'startTime' => $arrStart,
+        ]);
+    }
+    
 }
